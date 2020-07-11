39a4bf39a2e037bf819eee239d5d9dbfa0f68b26
VarHastra <alex.petrakov.dev@gmail.com>
2020-07-11 17:08:47 +0300

    Initial commit. Empty app up and running

---
+++ app/src/main/java/me/alex/pet/apps/focus/presentation/HostActivity.kt
@@ -0,0 +1,24 @@
+package me.alex.pet.apps.focus.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.alex.pet.apps.focus.R
+import me.alex.pet.apps.focus.databinding.ActivityHostBinding
+import me.alex.pet.apps.focus.presentation.timer.TimerFragment
+
+class HostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHostBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, TimerFragment())
+                    .commit()
+        }
+    }
+}
---
+++ app/src/main/java/me/alex/pet/apps/focus/presentation/timer/TimerFragment.kt
@@ -0,0 +1,25 @@
+package me.alex.pet.apps.focus.presentation.timer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.alex.pet.apps.focus.databinding.FragmentTimerBinding
+
+class TimerFragment : Fragment() {
+
+    private var _binding: FragmentTimerBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentTimerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
