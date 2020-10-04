5a812220a0f73845c7bf25b96d5f324b09f15aab
Alex Petrakov <alex.petrakov.dev@gmail.com>
2020-10-04 18:11:09 +0300

    Improve notifications design

--- app/src/main/java/me/alex/pet/apps/focus/presentation/notificationservice/Notifications.kt
+++ app/src/main/java/me/alex/pet/apps/focus/presentation/notificationservice/Notifications.kt
@@ -10,6 +10,7 @@ import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import me.alex.pet.apps.focus.R
+import me.alex.pet.apps.focus.common.extensions.getColorCompat
 import me.alex.pet.apps.focus.domain.SessionType
 import me.alex.pet.apps.focus.domain.Timer
 import me.alex.pet.apps.focus.presentation.HostActivity
@@ -39,8 +40,8 @@ class Notifications(private val context: Context) {
         val resetAction = newAction(NotificationAction.RESET, R.drawable.ic_action_reset, context.getString(R.string.app_action_reset))
         val sessionSwitchAction = newAction(NotificationAction.SWITCH_TO_NEXT_SESSION, R.drawable.ic_action_switch_session, context.getString(R.string.app_start_work_session))
         return newPomodoroNotificationBuilder()
-                .setContentTitle("Break is over")
-                .setContentText("Ready to start a new focus session?")
+                .setContentTitle(context.getString(R.string.notifications_break_is_over))
+                .setContentText(context.getString(R.string.app_ready_to_start_a_work_session))
                 .addAction(sessionSwitchAction)
                 .addAction(resetAction)
                 .build()
@@ -50,8 +51,8 @@ class Notifications(private val context: Context) {
         val resetAction = newAction(NotificationAction.RESET, R.drawable.ic_action_reset, context.getString(R.string.app_action_reset))
         val sessionSwitchAction = newAction(NotificationAction.SWITCH_TO_NEXT_SESSION, R.drawable.ic_action_switch_session, context.getString(R.string.app_start_break))
         return newPomodoroNotificationBuilder()
-                .setContentTitle("Focus session is finished")
-                .setContentText("Ready to take a break?")
+                .setContentTitle(context.getString(R.string.notifications_work_session_is_finished))
+                .setContentText(context.getString(R.string.app_ready_to_take_a_break))
                 .addAction(sessionSwitchAction)
                 .addAction(resetAction)
                 .build()
@@ -61,33 +62,35 @@ class Notifications(private val context: Context) {
         val pauseAction = newAction(NotificationAction.PAUSE, R.drawable.ic_action_pause, context.getString(R.string.app_action_pause))
         val resumeAction = newAction(NotificationAction.RESUME, R.drawable.ic_action_start, context.getString(R.string.app_action_resume))
         val resetAction = newAction(NotificationAction.RESET, R.drawable.ic_action_reset, context.getString(R.string.app_action_reset))
-        return newPomodoroNotificationBuilder().apply {
-            when (timerState) {
-                Timer.State.RUNNING -> {
-                    setContentTitle(remainingSeconds.toString())
-                    setContentText(type.toString())
-                    addAction(pauseAction)
-                }
-                Timer.State.PAUSED -> {
-                    setContentTitle("$remainingSeconds (paused)")
-                    setContentText(type.toString())
-                    addAction(resumeAction)
-                    addAction(resetAction)
-                }
-                else -> {
-                    setContentTitle(remainingSeconds.toString())
-                    setContentText(type.toString())
-                }
-            }
-        }.build()
+        val actions = when (timerState) {
+            Timer.State.RUNNING -> listOf(pauseAction)
+            Timer.State.PAUSED -> listOf(resumeAction, resetAction)
+            else -> emptyList()
+        }
+
+        val minutes = remainingSeconds / 60
+        val seconds = remainingSeconds % 60
+        val contentTitle = context.getString(R.string.app_duration_hours_seconds_format, minutes, seconds)
+        val contentText = when (type) {
+            SessionType.WORK -> R.string.notifications_work_session
+            else -> R.string.notifications_break
+        }.let { stringId ->
+            val text = context.getString(stringId)
+            if (timerState == Timer.State.PAUSED) context.getString(R.string.notifications_paused_format, text) else text
+        }
+
+        return newPomodoroNotificationBuilder()
+                .setContentTitle(contentTitle)
+                .setContentText(contentText).also { builder ->
+                    actions.forEach { builder.addAction(it) }
+                }.build()
     }
 
     private fun newPomodoroNotificationBuilder(): NotificationCompat.Builder {
         val intent = Intent(context, HostActivity::class.java)
         val tapActionIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        // TODO: Replace notification icon with a custom one once it's ready
-        // TODO: Set notification color
-        return NotificationCompat.Builder(context, TIMER_NOTIFICATION_CHANNEL_ID).setSmallIcon(R.drawable.ic_action_start)
+        return NotificationCompat.Builder(context, TIMER_NOTIFICATION_CHANNEL_ID).setSmallIcon(R.drawable.ic_notification_app_icon)
+                .setColor(context.getColorCompat(R.color.colorPrimary)) // TODO: resolve the color according to the app theme
                 .setShowWhen(false)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setChannelId(TIMER_NOTIFICATION_CHANNEL_ID)
